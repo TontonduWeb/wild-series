bb7c083c5f5e9f1eb78a2180ccca1efc570b8cc7
Matthieu <matthieudejean@hotmail.com>
2021-05-23 17:38:29 +0200

    Symfony - Le routing avance

--- src/Controller/DefautController.php
+++ src/Controller/DefautController.php
@@ -7,6 +7,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+    /**
+     * @Route("/defaut", name="defaut_")
+     */
 class DefautController extends AbstractController
 {
     /**
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -7,18 +7,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
+    /**
+     * @Route("/programs", name="program_")
+     */
 class ProgramController extends AbstractController
 {
     /**
-
-     * @Route("/programs/", name="program_index")
-
+     * @Route("/", name="program_index")
      */
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Séries',
         ]);
+    }
+
+    /**
+     * @Route("/{id}", methods={"GET"}, requirements={"id"="\d+"}, name="program_show")
+     */
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id]);
     }
 }
